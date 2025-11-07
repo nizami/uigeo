68c841a900278790522c97dfcb7dfc9ba8886eb4
nizami <nizami.dev@gmail.com>
2025-11-07 20:30:00 +0300

    Update demo project

--- projects/demo/src/app/app.ts
+++ projects/demo/src/app/app.ts
@@ -1,12 +1,11 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import { Cursor, Transformer, Canvas } from 'uigeo';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Transformer, Cursor, Canvas],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
-export class App {
-  protected readonly title = signal('demo');
-}
+export class App {}
--- projects/uigeo/src/public-api.ts
+++ projects/uigeo/src/public-api.ts
@@ -1,5 +1,6 @@
-/*
- * Public API Surface of uigeo
- */
-
-export * from './lib/uigeo';
+export * from './lib/components/canvas/canvas';
+export * from './lib/components/cursor/cursor';
+export * from './lib/components/transformer/editor-element-model';
+export * from './lib/components/transformer/transformer';
+export * from './lib/services/cursor.service';
+export * from './lib/util/text-case';
